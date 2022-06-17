3f93940368283ae4d7775284d0b470488db67a1c
rajeebsahoo <58513730+rajeebsahoo@users.noreply.github.com>
2022-06-17 17:39:37 +0530

    Update setup.sh
    sadcvcd

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 # Store workspace public IP to config file
+# I don't know anyth
 IP_VAR="$(curl http://checkip.amazonaws.com/)"
 CONFIG='{"workspaceIp": "'"$IP_VAR"'"}'
 echo $CONFIG > src/ipConfig.json
